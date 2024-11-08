282a2e13aad3e4c284f79757eb57039b896b3d19
evanperez444 <129793777+evanperez444@users.noreply.github.com>
2024-11-08 18:17:26 -0500

    Initial commit with Python file

---
+++ property_analytics.py
@@ -0,0 +1,8 @@
+from airflow import DAG
+from datetime import timedelta, datetime
+import json
+import requests
+from airflow.operators.python import PythonOperator
+from airflow.operators.bash_operator import BashOperator
+from airflow.providers.amazon.aws.sensors.s3 import S3KeySensor
+from airflow.providers.amazon.aws.transfers.s3_to_redshift import S3ToRedshiftOperator
